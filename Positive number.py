26074180e354608ba6b4558c57c9a14f377a1871
bebe-shannu <67957189+bebe-shannu@users.noreply.github.com>
2020-07-09 18:02:36 +0530

    Add files via upload

---
+++ Positive number.py
@@ -0,0 +1,14 @@
+
+#input1
+list1 = [12, -7,5,64,-14]
+for pn in list1:
+    if pn>=0:
+        print(pn)
+
+
+#input2
+list2 = [12, 14,-95,3]
+positivenumbers= [num for num in list2 if num>=0]
+print("output2: ", positivenumbers)
+
+
