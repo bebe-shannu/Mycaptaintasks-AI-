9e3cd9959467b7436ea8ea2904fe81ac3fd42d2d
bebe-shannu <67957189+bebe-shannu@users.noreply.github.com>
2020-07-10 19:24:21 +0530

    Add files via upload

---
+++ Python code snippets.py
@@ -0,0 +1,30 @@
+#Assigning elements to different lists
+
+#using append()
+bts_hyungline = ['kimnamjoon', 'kimsoekjin'] 
+bts_hyungline.append('minyoongi') 
+print ("BTS Hyungline: ",bts_hyungline)
+
+#using insert()
+
+bts_rapline = ['kimnamjoon', 'junghoseok'] 
+bts_rapline.insert(1,'minyoongi') 
+print ("BTS Rapline: ",bts_rapline)
+
+
+
+#Accessing elements  from a tuple
+
+
+oneDirection= ('Harry Styles', 'Louis Tomlinson ', 'Niall Horan' , 'Liam Payne');
+print("I love ",oneDirection[0])
+
+
+
+#Deleting different dictionary elements
+
+BT21 = {"Namjoon": 'Koya', 'Jin': 'RJ', 'Suga': 'Shooky', 'Jhope': 'Mang'}
+BT21.pop('Jin')
+print("BTS line characters BT21: ",BT21)
+
+
